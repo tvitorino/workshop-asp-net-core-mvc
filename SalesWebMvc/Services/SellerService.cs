526a564136d4368343f660807c31a8abdb358a98
tvitorino <taniasvitorino@gmail.com>
2019-05-14 20:02:37 +0100

    Simple create form

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -21,6 +21,12 @@ namespace SalesWebMvc.Services
 
         }
 
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
+
     }
 
 }
