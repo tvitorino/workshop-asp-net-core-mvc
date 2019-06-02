6abd182f18ac1179792f4069956f50fc3e076601
tvitorino <taniasvitorino@gmail.com>
2019-06-02 12:31:05 +0100

    details and stuff

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using SalesWebMvc.Models;
 using SalesWebMvc.Models.ViewModels;
 using SalesWebMvc.Services;
+using Microsoft.EntityFrameworkCore;
 
 namespace SalesWebMvc.Controllers
 {
@@ -64,5 +65,20 @@ namespace SalesWebMvc.Controllers
             _sellerService.Delete(id);
             return RedirectToAction(nameof(Index));
         }
+
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var obj = _sellerService.FindById(id.Value);
+
+            if (obj == null)
+                return NotFound();
+
+            return View(obj);
+        }
+
     }
 }
